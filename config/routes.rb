836decb7e7490a2300ad3c34deeffe45c91a96b8
nateobabs <64073805+nateobabs@users.noreply.github.com>
2023-06-26 21:36:07 +0100

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  get "/portfolio", to: "pages#portfolio"
 end
